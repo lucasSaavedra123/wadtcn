867dde727c30ee0762048a77869cc0278aedd5d9
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2023-04-26 10:42:06 -0300

    Code Updated

--- PredictiveModel/HurstExponentModel.py
+++ PredictiveModel/HurstExponentModel.py
@@ -11,6 +11,7 @@ from TheoreticalModels.LevyWalk import LevyWalk
 from TheoreticalModels.FractionalBrownianMotion import FractionalBrownianMotionBrownian, FractionalBrownianMotionSubDiffusive, FractionalBrownianMotionSuperDiffusive
 from TheoreticalModels.ScaledBrownianMotion import ScaledBrownianMotionBrownian, ScaledBrownianMotionSubDiffusive, ScaledBrownianMotionSuperDiffusive
 from .PredictiveModel import PredictiveModel
+from .model_utils import transform_trajectories_into_displacements
 
 class HurstExponentModel(PredictiveModel):
 
@@ -112,23 +113,7 @@ class HurstExponentModel(PredictiveModel):
         return Y
 
     def transform_trajectories_to_input(self, trajectories):
-        X = np.zeros((len(trajectories), self.trajectory_length-1, 2))
-
-        def axis_adaptation_to_net(axis_data, track_length):
-            axis_reshaped = np.reshape(axis_data, newshape=[1, len(axis_data)])
-            axis_reshaped = axis_reshaped - np.mean(axis_reshaped)
-            axis_diff = np.diff(axis_reshaped[0, :track_length])
-            return axis_diff
-
-        for index, trajectory in enumerate(trajectories):
-            X[index, :, 0] = axis_adaptation_to_net(trajectory.get_noisy_x(), self.trajectory_length)
-            X[index, :, 1] = axis_adaptation_to_net(trajectory.get_noisy_y(), self.trajectory_length)
-
-            if self.simulator().STRING_LABEL == 'andi':
-                X[index, :, 0] = (X[index, :, 0] - np.mean(X[index, :, 0]))/np.std(X[index, :, 0])
-                X[index, :, 1] = (X[index, :, 1] - np.mean(X[index, :, 1]))/np.std(X[index, :, 1])
-
-        return X
+        return transform_trajectories_into_displacements(self, trajectories)
 
     def build_network(self):
         inputs = Input(shape=(self.trajectory_length, 1))
