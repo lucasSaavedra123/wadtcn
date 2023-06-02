713e4f8906c43c2deaf24e8ed3a407a09a9fc03c
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-06-01 23:48:26 -0300

    Hyperparameter Analysis Code Updated

--- hyperparameter_analysis.py
+++ hyperparameter_analysis.py
@@ -8,19 +8,18 @@ from PredictiveModel.WaveNetTCNTheoreticalModelClassifier import WaveNetTCNTheor
 from TheoreticalModels import ALL_SUB_MODELS
 
 
-TRAJECTORY_LENGTHS = [25, 50]
-
 DatabaseHandler.connect_to_local('anomalous_diffusion')
 
-for trajectory_length in TRAJECTORY_LENGTHS:
-    for predictive_model_class in [
-        WaveNetTCNTheoreticalModelClassifier,
-        WaveNetTCNFBMModelClassifier,
-        WaveNetTCNSBMModelClassifier
-    ]:
-        predictive_model_class.analyze_hyperparameters(trajectory_length, trajectory_length, initial_epochs=5, steps=5, simulator=AndiDataSimulation)
+trajectory_length = 25
 
-    for class_model in ALL_SUB_MODELS:
-        WavenetTCNWithLSTMHurstExponentPredicter(trajectory_length, trajectory_length, initial_epochs=5, steps=5, simulator=AndiDataSimulation, model=class_model.STRING_LABEL)
+for predictive_model_class in [
+    WaveNetTCNTheoreticalModelClassifier,
+    WaveNetTCNFBMModelClassifier,
+    WaveNetTCNSBMModelClassifier
+]:
+    predictive_model_class.analyze_hyperparameters(trajectory_length, trajectory_length, initial_epochs=5, steps=5, simulator=AndiDataSimulation)
+
+for class_model in ALL_SUB_MODELS:
+    WavenetTCNWithLSTMHurstExponentPredicter(trajectory_length, trajectory_length, initial_epochs=5, steps=5, simulator=AndiDataSimulation, model=class_model.STRING_LABEL)
 
 DatabaseHandler.disconnect()
