b42b307aa628528c108111664838cf6444072863
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-08-08 13:58:39 -0300

    Code Updated

--- PredictiveModel/LSTMAnomalousExponentPredicter.py
+++ PredictiveModel/LSTMAnomalousExponentPredicter.py
@@ -1,59 +1,93 @@
 import numpy as np
-from keras.layers import Dense, Input, LSTM
-from keras.models import Model
+from keras.models import load_model
 
+from .model_utils import transform_trajectories_to_anomalous_exponent
+from CONSTANTS import *
+from .randi_utils import *
 from .PredictiveModel import PredictiveModel
 
-from .model_utils import transform_trajectories_into_displacements, transform_trajectories_to_anomalous_exponent
 
 class LSTMAnomalousExponentPredicter(PredictiveModel):
+    @classmethod
+    def selected_hyperparameters(self):
+        return {'None': 'None'}
+
     def default_hyperparameters(self):
-        return {
-            'batch_size': 32,
-            'amsgrad': False,
-            'epsilon': 1e-6,
-            'epochs': 100,
-            'lr': 0.01
-        }
+        return {'None': 'None'}
 
     @classmethod
     def default_hyperparameters_analysis(self):
-        pass
-
+        return None
 
     """
-    This network comes from paper:
+    This classification comes from paper:
 
     Aykut Argun, Giovanni Volpe, Stefano Bo
 
     Classification, Inference, and Segmentation of anomalous
     diffusion with recurrent neural networks
 
-    Original code: https://github.com/argunaykut/randi/blob/main/inference_train_network.ipynb
+    Original code: https://github.com/argunaykut/randi/blob/main/using_the_nets.ipynb
     """
-    def build_network(self):
-        inputs = Input((self.trajectory_length-1, 2))
+    @classmethod
+    def classify_with_combination(cls, trajectories, list_of_classifiers):
+        N = len(trajectories)
+        dimension = 2
+        traj_length = trajectories[0].length
 
-        x = LSTM(250, return_sequences=True)(inputs)
-        x = LSTM(50)(x)
-        outputs = Dense(1)(x)
+        X = np.zeros((N, traj_length * dimension))
 
-        self.architecture = Model(inputs=inputs, outputs=outputs)                             
+        for i in range(N):
+            X[i,:traj_length] = trajectories[i].get_noisy_x()
+            X[i,traj_length:] = trajectories[i].get_noisy_y()
 
-        self.architecture.compile(optimizer='adam', loss="mse", metrics=["mae"])
+        centers_class_2d = [classifier.trajectory_length for classifier in list_of_classifiers]
+        meta_model_class_2d = [classifier.architecture for classifier in list_of_classifiers]
 
-    def predict(self, trajectories):
-        X = self.transform_trajectories_to_input(trajectories)
-        Y_predicted = self.architecture.predict(X)
-        Y_predicted = np.argmax(Y_predicted, axis=-1)
+        Y_predicted = many_net_uhd(nets = meta_model_class_2d, traj_set = X, centers = centers_class_2d ,dim = 2, task = 1)
+
         return Y_predicted
+
+    """
+    This transformation comes from paper:
+
+    Aykut Argun, Giovanni Volpe, Stefano Bo
+
+    Classification, Inference, and Segmentation of anomalous
+    diffusion with recurrent neural networks
+
+    Original code: https://github.com/argunaykut/randi/blob/main/using_the_nets.ipynb
+    """
+    def transform_trajectories_to_input(self, trajectories):
+        N = len(trajectories)
+        dimension = 2
+        traj_length = trajectories[0].length
+
+        X = np.zeros((N, traj_length * dimension))
+
+        for i in range(len(trajectories)):
+            X[i,:traj_length] = trajectories[i].get_noisy_x()
+            X[i,traj_length:] = trajectories[i].get_noisy_y()
+
+        block_size = self.architecture.layers[0].input_shape[-1]
+
+        data = data_norm(X,dim=2,task=1)
+        data_rs = data_reshape(data,bs=block_size,dim=2)
+
+        return data_rs
 
     def transform_trajectories_to_output(self, trajectories):
         return transform_trajectories_to_anomalous_exponent(self, trajectories)
 
-    def transform_trajectories_to_input(self, trajectories):
-        return transform_trajectories_into_displacements(self, trajectories)
-
     @property
     def type_name(self):
-        return f"lstm_anomalous_exponent_inference"
+        return f"randi_inference"
+
+    def __str__(self):
+        return f"{self.type_name}_{self.trajectory_length}"
+    
+    def save_as_file(self):
+        raise Exception("RANDI is not persisted in MongoDB")
+
+    def load_as_file(self):
+        self.architecture = load_model(f'{str(self)}.h5')
