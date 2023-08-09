8e496cf1be0581c678ec9ee83ba64f73d6c281be
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-08-09 10:54:13 -0300

    Code Updated

--- wadnet_inference_andi_training_script.py
+++ wadnet_inference_andi_training_script.py
@@ -6,6 +6,7 @@ from DataSimulation import AndiDataSimulation
 from PredictiveModel.WaveNetTCNFBMModelClassifier import WaveNetTCNFBMModelClassifier
 from PredictiveModel.WaveNetTCNSBMModelClassifier import WaveNetTCNSBMModelClassifier
 from PredictiveModel.WavenetTCNWithLSTMHurstExponentPredicter import WavenetTCNWithLSTMHurstExponentPredicter
+from TheoreticalModels import ALL_SUB_MODELS
 
 DatabaseHandler.connect_over_network(None, None, '10.147.20.1', 'anomalous_diffusion_models')
 
@@ -15,24 +16,40 @@ for length in tqdm.tqdm(lengths):
     for network_class in [WaveNetTCNFBMModelClassifier, WaveNetTCNSBMModelClassifier, WavenetTCNWithLSTMHurstExponentPredicter]:
 
         if network_class == WavenetTCNWithLSTMHurstExponentPredicter:
-            #already_trained_networks = network_class.objects(simulator_identifier=AndiDataSimulation.STRING_LABEL, trained=True, hyperparameters=WavenetTCNWithLSTMHurstExponentPredicter.selected_hyperparameters())
-            pass
+
+            for class_model in ALL_SUB_MODELS:
+                already_trained_networks = network_class.objects(simulator_identifier=AndiDataSimulation.STRING_LABEL, model=class_model.STRING_LABEL, trained=True, hyperparameters=network_class.selected_hyperparameters())
+
+                networks_of_length = [network for network in already_trained_networks if network.trajectory_length == length]
+
+                if len(networks_of_length) == 0:
+                    classifier = network_class(length, length, model=class_model.STRING_LABEL, simulator=AndiDataSimulation)
+                    classifier.enable_early_stopping()
+                    classifier.enable_database_persistance()
+                    classifier.fit()
+                    classifier.save()
+                else:
+                    assert len(networks_of_length) == 1
+                    classifier = networks_of_length[0]
+                    classifier.enable_database_persistance()
+                    classifier.load_as_file()
+
         else:
             already_trained_networks = network_class.objects(simulator_identifier=AndiDataSimulation.STRING_LABEL, trained=True, hyperparameters=network_class.selected_hyperparameters())
 
-        networks_of_length = [network for network in already_trained_networks if network.trajectory_length == length]
+            networks_of_length = [network for network in already_trained_networks if network.trajectory_length == length]
 
-        if len(networks_of_length) == 0:
-            classifier = network_class(length, length, simulator=AndiDataSimulation)
-            classifier.enable_early_stopping()
-            classifier.enable_database_persistance()
-            classifier.fit()
-            classifier.save()
-        else:
-            assert len(networks_of_length) == 1
-            classifier = networks_of_length[0]
-            classifier.enable_database_persistance()
-            classifier.load_as_file()
+            if len(networks_of_length) == 0:
+                classifier = network_class(length, length, simulator=AndiDataSimulation)
+                classifier.enable_early_stopping()
+                classifier.enable_database_persistance()
+                classifier.fit()
+                classifier.save()
+            else:
+                assert len(networks_of_length) == 1
+                classifier = networks_of_length[0]
+                classifier.enable_database_persistance()
+                classifier.load_as_file()
 
 
 DatabaseHandler.disconnect()
