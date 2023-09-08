1dc2da3af1539e451c822fe9f865dd0b073f80d9
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-09-07 22:02:09 -0300

    Code Updated

--- PredictiveModel/WavenetTCNWithLSTMDiffusionCoefficientFBMPredicter.py
+++ PredictiveModel/WavenetTCNWithLSTMDiffusionCoefficientFBMPredicter.py
@@ -2,45 +2,30 @@ from keras.layers import Dense, Input, GlobalAveragePooling1D
 from keras.models import Model
 from tensorflow.keras.optimizers.legacy import Adam
 
-from TheoreticalModels.FractionalBrownianMotion import FractionalBrownianMotionBrownian, FractionalBrownianMotionSubDiffusive, FractionalBrownianMotionSuperDiffusive
+from TheoreticalModels.FractionalBrownianMotion import FractionalBrownianMotion
 from .PredictiveModel import PredictiveModel
 from .model_utils import *
 from CONSTANTS import *
 
-class WavenetTCNWithLSTMDiffusionCoefficientPredicter(PredictiveModel):
-    #These will be updated after hyperparameter search
-
-    def model_string_to_class_dictionary(self):
+class WavenetTCNWithLSTMDiffusionCoefficientFBMPredicter(PredictiveModel):
+    def default_hyperparameters(self):
         return {
-            'fbm_sub': { 'model': FractionalBrownianMotionSubDiffusive },
-            'fbm_brownian': { 'model': FractionalBrownianMotionBrownian },
-            'fbm_sup': { 'model': FractionalBrownianMotionSuperDiffusive },
+            'lr': 0.0001,
+            'batch_size': 8,
+            'amsgrad': False,
+            'epsilon': 1e-08,
+            'epochs': 100
         }
-
-    def default_hyperparameters(self, **kwargs):
-        model_string_to_hyperparameters_dictionary = {
-            'fbm_sub': {'lr': 0.0001, 'batch_size': 64, 'amsgrad': False, 'epsilon': 1e-08, 'epochs': 100},
-            'fbm_brownian': {'lr': 0.001, 'batch_size': 16, 'amsgrad': True, 'epsilon': 1e-06, 'epochs': 100},
-            'fbm_sup': {'lr': 0.0001, 'batch_size': 8, 'amsgrad': False, 'epsilon': 1e-08, 'epochs': 100},
-        }
-
-        hyperparameters = model_string_to_hyperparameters_dictionary[kwargs["model"]]
-        hyperparameters['epochs'] = 100
-
-        return hyperparameters
 
     @classmethod
-    def selected_hyperparameters(self, model_label):
-        model_string_to_hyperparameters_dictionary = {
-            'fbm_sub': {'lr': 0.0001, 'batch_size': 64, 'amsgrad': False, 'epsilon': 1e-08, 'epochs': 100},
-            'fbm_brownian': {'lr': 0.001, 'batch_size': 16, 'amsgrad': True, 'epsilon': 1e-06, 'epochs': 100},
-            'fbm_sup': {'lr': 0.0001, 'batch_size': 8, 'amsgrad': False, 'epsilon': 1e-08, 'epochs': 100},
+    def selected_hyperparameters(self):
+        return {
+            'lr': 0.0001,
+            'batch_size': 8,
+            'amsgrad': False,
+            'epsilon': 1e-08,
+            'epochs': 100
         }
-
-        hyperparameters = model_string_to_hyperparameters_dictionary[model_label]
-        hyperparameters['epochs'] = 100
-
-        return hyperparameters
 
     @classmethod
     def default_hyperparameters_analysis(self):
@@ -53,7 +38,7 @@ class WavenetTCNWithLSTMDiffusionCoefficientPredicter(PredictiveModel):
 
     @property
     def models_involved_in_predictive_model(self):
-        return [self.model_string_to_class_dictionary()[self.extra_parameters["model"]]['model']]
+        return [FractionalBrownianMotion]
 
     def predict(self, trajectories):
         return self.architecture.predict(self.transform_trajectories_to_input(trajectories))
@@ -66,7 +51,7 @@ class WavenetTCNWithLSTMDiffusionCoefficientPredicter(PredictiveModel):
 
     def build_network(self):
         inputs = Input(shape=(self.trajectory_length-1, 1))
-        filters = 32
+        filters = 64
         dilation_depth = 8
         initializer = 'he_normal'
 
