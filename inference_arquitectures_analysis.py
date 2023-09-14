b396476db8871ac289ebb618f2868fc377e1fce9
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-09-14 14:06:30 -0300

    Inference Analyzer updated

--- inference_arquitectures_analysis.py
+++ inference_arquitectures_analysis.py
@@ -1,17 +1,18 @@
 import tqdm
 import pandas as pd
+import numpy as np
 
 from CONSTANTS import *
-from TheoreticalModels import ANDI_MODELS
+from TheoreticalModels import *
 from DataSimulation import AndiDataSimulation
-from PredictiveModel.model_utils import transform_trajectories_to_anomalous_exponent
-from sklearn.metrics import f1_score, mean_absolute_error
-
+from PredictiveModel.model_utils import transform_trajectories_to_anomalous_exponent, plot_predicted_and_ground_truth_histogram
+from sklearn.metrics import mean_absolute_error
 from PredictiveModel.LSTMAnomalousExponentPredicter import LSTMAnomalousExponentPredicter
 from PredictiveModel.inference_utils import get_architectures_for_inference,  infer_with_concatenated_networks
 
 from DatabaseHandler import DatabaseHandler
 from DataSimulation import AndiDataSimulation
+
 
 randi_lengths = [25,65,125,225,325,425,525,725,925]
 randi_classifiers = []
@@ -21,6 +22,18 @@ length_and_mae = {
     'mae_wadtcn': [],
     'mae_original': [],
     'mae_lstm': [],
+}
+
+theoretical_model_and_mae = {
+    'mae_wadtcn': {t_model.STRING_LABEL:{'ground_truth':[], 'predictions': []} for t_model in ANDI_MODELS},
+    'mae_original': {t_model.STRING_LABEL:{'ground_truth':[], 'predictions': []} for t_model in ANDI_MODELS},
+    'mae_lstm': {t_model.STRING_LABEL:{'ground_truth':[], 'predictions': []} for t_model in ANDI_MODELS},
+}
+
+all_info = {
+    'mae_wadtcn': {'ground_truth':[], 'predictions': []},
+    'mae_original': {'ground_truth':[], 'predictions': []},
+    'mae_lstm': {'ground_truth':[], 'predictions': []},
 }
 
 print("Loading RANDI networks...")
@@ -54,7 +67,6 @@ for length in tqdm.tqdm(lengths):
         else:
             raise msg
 
-
 for length in tqdm.tqdm(lengths):
     trajectories = AndiDataSimulation().simulate_trajectories_by_model(12500, length, length, ANDI_MODELS)
 
@@ -66,8 +78,8 @@ for length in tqdm.tqdm(lengths):
     ):
         
         if info[1] == LSTMAnomalousExponentPredicter:            
-            predictions = LSTMAnomalousExponentPredicter.classify_with_combination(trajectories, randi_classifiers)
-            ground_truth = transform_trajectories_to_anomalous_exponent(classifier, trajectories)
+            predictions = LSTMAnomalousExponentPredicter.classify_with_combination(trajectories, randi_classifiers).tolist()
+            ground_truth = transform_trajectories_to_anomalous_exponent(classifier, trajectories)[:,0].tolist()
         else:
             try:
                 dictionary_to_use = length_to_custom_networks[length] if info[1] == 'wadtcn' else length_to_original_networks[length]
@@ -78,7 +90,82 @@ for length in tqdm.tqdm(lengths):
 
         length_and_mae[info[0]].append(mean_absolute_error(ground_truth, predictions))
 
+        for i in range(len(predictions)):
+            theoretical_model_and_mae[info[0]][trajectories[i].model_category.__class__.STRING_LABEL]['predictions'].append(predictions[i])
+            theoretical_model_and_mae[info[0]][trajectories[i].model_category.__class__.STRING_LABEL]['ground_truth'].append(ground_truth[i])
+
     pd.DataFrame(length_and_mae).to_csv('length_inference_result.csv', index=False)
 
+for andi_model in ANDI_MODELS:
+    for arquitecture_name in ('mae_wadtcn', 'mae_lstm', 'mae_original'):
+        p = theoretical_model_and_mae[arquitecture_name][andi_model.STRING_LABEL]['predictions']
+        g = theoretical_model_and_mae[arquitecture_name][andi_model.STRING_LABEL]['ground_truth']
+
+        theoretical_model_and_mae[arquitecture_name][andi_model.STRING_LABEL] = mean_absolute_error(g, p)
+
+to_save_theoretical_model_and_mae = {
+    'model': [],
+    'mae_wadtcn': [],
+    'mae_original': [],
+    'mae_lstm': [],
+}
+
+for andi_model in ANDI_MODELS:
+    to_save_theoretical_model_and_mae['model'].append(andi_model.STRING_LABEL)
+    for arquitecture_name in ('mae_wadtcn', 'mae_lstm', 'mae_original'):
+        to_save_theoretical_model_and_mae[arquitecture_name].append(theoretical_model_and_mae[arquitecture_name][andi_model.STRING_LABEL])
+
+pd.DataFrame(to_save_theoretical_model_and_mae).to_csv('model_inference_result.csv', index=False)
+
+trajectories_by_length = {}
+
+for trajectory_id in range(12500):
+    alpha = np.random.uniform(0.05,1.95)
+
+    if alpha < 0.95:
+        choice_models = SUB_DIFFUSIVE_MODELS
+    elif 0.95 <= alpha < 1.05:
+        choice_models = BROWNIAN_MODELS
+    else:
+        choice_models = SUP_DIFFUSIVE_MODELS
+
+    model = np.random.choice(choice_models)
+    model_instance = model.create_random_instance()
+
+    if model == FractionalBrownianMotion:
+        model_instance.hurst_exponent = alpha/2
+    else:
+        model_instance.anomalous_exponent = alpha
+
+    selected_length = np.random.choice(lengths)
+    trajectory = model_instance.simulate_trajectory(selected_length, selected_length, from_andi=True)
+
+    if selected_length not in trajectories_by_length:
+        trajectories_by_length[selected_length] = []
+    
+    trajectories_by_length[selected_length].append(trajectory)
+
+for info in zip(
+    ('mae_wadtcn', 'mae_lstm', 'mae_original'),
+    ('wadtcn', LSTMAnomalousExponentPredicter, 'original')
+):
+    for trajectories in trajectories_by_length.values():
+        if info[1] == LSTMAnomalousExponentPredicter:            
+            predictions = LSTMAnomalousExponentPredicter.classify_with_combination(trajectories, randi_classifiers).tolist()
+            ground_truth = transform_trajectories_to_anomalous_exponent(classifier, trajectories)[:,0].tolist()
+        else:
+            try:
+                dictionary_to_use = length_to_custom_networks[length] if info[1] == 'wadtcn' else length_to_original_networks[length]
+                ground_truth, predictions = infer_with_concatenated_networks(dictionary_to_use, trajectories, return_ground_truth=True)
+            except KeyError:
+                ground_truth = [0]
+                predictions = [0]
+
+        for i in range(len(predictions)):
+            all_info[info[0]]['predictions'].append(predictions[i])
+            all_info[info[0]]['ground_truth'].append(ground_truth[i])    
+
+for arquitecture_name in all_info:
+    plot_predicted_and_ground_truth_histogram(all_info[arquitecture_name]['ground_truth'], all_info[arquitecture_name]['predictions'], range=[[0,2], [0,2]], title=arquitecture_name, save=True)
 
 DatabaseHandler.disconnect()
