4a74326d99bf50f6d4b21990dd427daa965ae9bf
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-09-21 22:06:13 -0300

    Code Updated

--- wadnet_classification_andi_comparison_of_weights.py
+++ wadnet_classification_andi_comparison_of_weights.py
@@ -12,19 +12,19 @@ f1_scores = []
 DatabaseHandler.connect_over_network(None, None, '10.147.20.1', 'anomalous_diffusion_models')
 
 already_trained_networks = WaveNetTCNTheoreticalModelClassifier.objects(simulator_identifier=AndiDataSimulation.STRING_LABEL, trained=True, hyperparameters=WaveNetTCNTheoreticalModelClassifier.selected_hyperparameters())
-reference_length_length_classifier = [network for network in already_trained_networks if network.trajectory_length == REFERENCE_LENGTH][0]
-reference_length_length_classifier.enable_database_persistance()
-reference_length_length_classifier.load_as_file()
+reference_classifier = [network for network in already_trained_networks if network.trajectory_length == REFERENCE_LENGTH][0]
+reference_classifier.enable_database_persistance()
+reference_classifier.load_as_file()
 
 for length in tqdm.tqdm(LENGTHS):
     if length == REFERENCE_LENGTH:
-        f1_scores.append(reference_length_length_classifier.micro_f1_score())
+        f1_scores.append(reference_classifier.micro_f1_score())
     else:
         classifier = [network for network in already_trained_networks if network.trajectory_length == length][0]
         classifier.enable_database_persistance()
         classifier.load_as_file()
 
-        classifier.architecture.set_weights(reference_length_length_classifier.architecture.get_weights())
+        classifier.architecture.set_weights(reference_classifier.architecture.get_weights())
         f1_scores.append(classifier.micro_f1_score())
 
 pd.DataFrame({
