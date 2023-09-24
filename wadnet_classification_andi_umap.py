32025d9049940d608871bb894421b8fc6bb3f209
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-09-24 20:30:18 -0300

    Code Updated

--- wadnet_classification_andi_umap.py
+++ wadnet_classification_andi_umap.py
@@ -1,5 +1,3 @@
-import tqdm
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Model
@@ -7,8 +5,8 @@ import seaborn as sns
 import umap
 
 from DatabaseHandler import DatabaseHandler
-from DataSimulation import AndiDataSimulation
-from TheoreticalModels import ANDI_MODELS
+from DataSimulation import AndiDataSimulation, CustomDataSimulation
+from TheoreticalModels import ALL_MODELS, ANDI_MODELS
 from PredictiveModel.WaveNetTCNTheoreticalModelClassifier import WaveNetTCNTheoreticalModelClassifier
 
 
@@ -21,62 +19,56 @@ def get_encoder_from_classifier(a_classifier):
 
     return encoding_model
 
-REFERENCE_LENGTH_ONE = 975
-REFERENCE_LENGTH_TWO = 500
+reference_lengths = [25,500,975]
+simulator_reference = CustomDataSimulation
+
+length_to_encoder = {}
 
 print(f"Obtaining reference models...")
 DatabaseHandler.connect_over_network(None, None, '10.147.20.1', 'anomalous_diffusion_models')
-already_trained_networks = WaveNetTCNTheoreticalModelClassifier.objects(
-    simulator_identifier=AndiDataSimulation.STRING_LABEL,
-    trained=True,
-    hyperparameters=WaveNetTCNTheoreticalModelClassifier.selected_hyperparameters()
-)
-reference_one_classifier = [network for network in already_trained_networks if network.trajectory_length == REFERENCE_LENGTH_ONE][0]
-reference_one_classifier.enable_database_persistance()
-reference_one_classifier.load_as_file()
-DatabaseHandler.disconnect()
 
-reference_length_one_encoder = get_encoder_from_classifier(reference_one_classifier)
-
-reference_two_classifier = WaveNetTCNTheoreticalModelClassifier(REFERENCE_LENGTH_TWO,REFERENCE_LENGTH_TWO, simulator=AndiDataSimulation)
-reference_two_classifier.build_network()
-reference_length_two_encoder = get_encoder_from_classifier(reference_two_classifier)
-
-reference_length_two_encoder.set_weights(reference_length_one_encoder.get_weights())
-
-length_to_encoder = {
-    REFERENCE_LENGTH_ONE: reference_length_one_encoder,
-    REFERENCE_LENGTH_TWO: reference_length_two_encoder
-}
-
-length_to_classifier = {
-    REFERENCE_LENGTH_ONE: reference_one_classifier,
-    REFERENCE_LENGTH_TWO: reference_two_classifier
-}
-
-print("Encoding with lengths...")
-
-for length in [REFERENCE_LENGTH_ONE, REFERENCE_LENGTH_TWO]:
-    print(f"Simulating trajectories... (L={length})")
-    trajectories = AndiDataSimulation().simulate_trajectories_by_model(12500, length, 12500, ANDI_MODELS)
-    print(f"Converting trajectories... (L={length})")
-    trajectories_transformed = length_to_classifier[length].transform_trajectories_to_input(trajectories)
-    trajectories_labels = np.argmax(length_to_classifier[length].transform_trajectories_to_output(trajectories),axis=1)
-    trajectories_encoded = length_to_encoder[length].predict(trajectories_transformed)
-
-    reducer = umap.UMAP()
-    embedding = reducer.fit_transform(trajectories_encoded)
-
-    plt.scatter(
-        embedding[:, 0],
-        embedding[:, 1],
-        s=1,
-        c=[sns.color_palette()[x] for x in trajectories_labels]
+for reference_length in reference_lengths:
+    already_trained_networks = WaveNetTCNTheoreticalModelClassifier.objects(
+        simulator_identifier=AndiDataSimulation.STRING_LABEL,
+        trained=True,
+        hyperparameters=WaveNetTCNTheoreticalModelClassifier.selected_hyperparameters()
     )
-    plt.gca().set_aspect('equal', 'datalim')
-    plt.tight_layout()
-    plt.xticks([])
-    plt.yticks([])
-    #plt.title('UMAP projection of the Penguin dataset', fontsize=24)
-    plt.savefig(f"{REFERENCE_LENGTH_ONE}_{length}.png")
-    plt.clf()
+    reference_classifier = [network for network in already_trained_networks if network.trajectory_length == reference_length][0]
+    reference_classifier.enable_database_persistance()
+    reference_classifier.load_as_file()
+    reference_classifier.simulator = simulator_reference
+    reference_encoder = get_encoder_from_classifier(reference_classifier)
+
+    for length in reference_lengths:
+        print(reference_length, "->", length)
+
+        length_classifier = WaveNetTCNTheoreticalModelClassifier(length,length, simulator=simulator_reference)
+        length_classifier.build_network()
+        length_encoder = get_encoder_from_classifier(length_classifier)
+
+        print("Transfering...")
+        length_encoder.set_weights(reference_encoder.get_weights())
+
+        print("Simulating Trajectories...")
+        trajectories = simulator_reference().simulate_trajectories_by_model(12500, length, 12500, ALL_MODELS if simulator_reference == CustomDataSimulation else ANDI_MODELS)
+        trajectories_transformed = length_classifier.transform_trajectories_to_input(trajectories)
+        trajectories_labels = np.argmax(length_classifier.transform_trajectories_to_output(trajectories),axis=1)
+        print("Encoding Trajectories...")
+        trajectories_encoded = length_encoder.predict(trajectories_transformed)
+
+        embedding = umap.UMAP().fit_transform(trajectories_encoded)
+
+        plt.scatter(
+            embedding[:, 0],
+            embedding[:, 1],
+            s=1,
+            c=[sns.color_palette()[x] for x in trajectories_labels]
+        )
+        plt.gca().set_aspect('equal', 'datalim')
+        plt.tight_layout()
+        plt.xticks([])
+        plt.yticks([])
+        plt.savefig(f"{reference_length}_{length}_{simulator_reference.STRING_LABEL}.png")
+        plt.clf()
+
+DatabaseHandler.disconnect()
