bb37a1c2dc49d4ff93f5c22ec812f092883b0fea
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-09-27 19:24:01 -0300

    Trajectories loader updated

--- load_trayectories_to_database.py
+++ load_trayectories_to_database.py
@@ -6,7 +6,7 @@ from Trajectory import Trajectory
 
 DatabaseHandler.connect_over_network(None, None, '10.147.20.1', 'anomalous_diffusion_analysis')
 
-mat_data = loadmat('data/all_tracks_thunder_localizer.mat')
+mat_data = loadmat('all_tracks_thunder_localizer.mat')
 # Orden en la struct [BTX|mAb] [CDx|Control|CDx-Chol]
 dataset = []
 # Add each label and condition to the dataset
@@ -29,9 +29,8 @@ dataset.append({'label': 'mAb',
                 'exp_cond': 'CDx-Chol',
                 'tracks': mat_data['tracks'][1][2]})
 
-if len(Trajectory.objects()) != 0:
-    for i in Trajectory.objects():
-        i.delete()
+for i in Trajectory.objects():
+    i.delete()
 
 for data in dataset:
     print(f"Label: {data['label']}, Experimental Condition: {data['exp_cond']}")
