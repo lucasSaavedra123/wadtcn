acba8efa056b9de5f743af7ff7b662ed6c88aba7
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-09-27 18:57:39 -0300

    Code Updated

--- inference_arquitectures_analysis.py
+++ inference_arquitectures_analysis.py
@@ -98,31 +98,34 @@ for length in tqdm.tqdm(lengths):
     pd.DataFrame(length_and_mae).to_csv('length_inference_result.csv', index=False)
 
 #Overall performance across different models
-trajectories = []
+trajectories_by_length = {length: [] for length in lengths}
 
 for trajectory_id in range(12500):
     selected_length = np.random.choice(lengths)
-    trajectories.append(AndiDataSimulation().simulate_trajectories_by_model(1, selected_length, selected_length, ANDI_MODELS)[0])
+    trajectories_by_length[selected_length].append(AndiDataSimulation().simulate_trajectories_by_model(1, selected_length, selected_length, ANDI_MODELS)[0])
 
-for info in zip(
-    ('mae_wadtcn', 'mae_lstm', 'mae_original'),
-    ('wadtcn', LSTMAnomalousExponentPredicter, 'original')
-):
+for length in trajectories_by_length:
+    trajectories = trajectories_by_length[length]
 
-    if info[1] == LSTMAnomalousExponentPredicter:            
-        predictions = LSTMAnomalousExponentPredicter.classify_with_combination(trajectories, randi_classifiers).tolist()
-        ground_truth = transform_trajectories_to_anomalous_exponent(classifier, trajectories)[:,0].tolist()
-    else:
-        try:
-            dictionary_to_use = length_to_custom_networks[length] if info[1] == 'wadtcn' else length_to_original_networks[length]
-            ground_truth, predictions = infer_with_concatenated_networks(dictionary_to_use, trajectories, return_ground_truth=True)
-        except KeyError:
-            ground_truth = [0]
-            predictions = [0]
+    for info in zip(
+        ('mae_wadtcn', 'mae_lstm', 'mae_original'),
+        ('wadtcn', LSTMAnomalousExponentPredicter, 'original')
+    ):
 
-    for i in range(len(predictions)):
-        theoretical_model_and_mae[info[0]][trajectories[i].model_category.__class__.STRING_LABEL]['predictions'].append(predictions[i])
-        theoretical_model_and_mae[info[0]][trajectories[i].model_category.__class__.STRING_LABEL]['ground_truth'].append(ground_truth[i])
+        if info[1] == LSTMAnomalousExponentPredicter:            
+            predictions = LSTMAnomalousExponentPredicter.classify_with_combination(trajectories, randi_classifiers).tolist()
+            ground_truth = transform_trajectories_to_anomalous_exponent(classifier, trajectories)[:,0].tolist()
+        else:
+            try:
+                dictionary_to_use = length_to_custom_networks[length] if info[1] == 'wadtcn' else length_to_original_networks[length]
+                ground_truth, predictions = infer_with_concatenated_networks(dictionary_to_use, trajectories, return_ground_truth=True)
+            except KeyError:
+                ground_truth = [0]
+                predictions = [0]
+
+        for i in range(len(predictions)):
+            theoretical_model_and_mae[info[0]][trajectories[i].model_category.__class__.STRING_LABEL]['predictions'].append(predictions[i])
+            theoretical_model_and_mae[info[0]][trajectories[i].model_category.__class__.STRING_LABEL]['ground_truth'].append(ground_truth[i])
 
 for andi_model in ANDI_MODELS:
     for arquitecture_name in ('mae_wadtcn', 'mae_lstm', 'mae_original'):
