6e4bdf55faff4c715799b5d6e4c4c572860b0ff2
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-10-10 13:49:32 -0300

    wadnet_inference_custom_training_script.py updated

--- wadnet_inference_custom_training_script.py
+++ wadnet_inference_custom_training_script.py
@@ -15,14 +15,16 @@ from CONSTANTS import IMMOBILE_THRESHOLD, EXPERIMENT_TIME_FRAME_BY_FRAME
 
 DatabaseHandler.connect_over_network(None, None, '10.147.20.1', 'anomalous_diffusion_analysis')
 
-if not os.path.exists('lengths_cache.txt'):
-    lengths = np.sort(np.unique([int(trajectory.length) for trajectory in Trajectory.objects() if (not trajectory.is_immobile(IMMOBILE_THRESHOLD)) and trajectory.length >= 25]))
+if not os.path.exists('lengths_and_durations_cache.txt'):
+    lengths_durations = [(trajectory.length, round(trajectory.duration,2)) for trajectory in Trajectory.objects() if (not trajectory.is_immobile(IMMOBILE_THRESHOLD)) and trajectory.length >= 25]
+    lengths_durations = list(set(lengths_durations))
+    lengths_durations = sorted(lengths_durations, key=lambda x: (x[0], -x[1]))
 
-    with open('lengths_cache.txt', 'w') as file:
-        file.write('\n'.join(str(length) for length in lengths))
+    with open('lengths_and_durations_cache.txt', 'w') as file:
+        file.write('\n'.join(str(length_duration[0])+','+str(length_duration[1]) for length_duration in lengths_durations))
 else:
-    with open('lengths_cache.txt', 'r') as file:
-        lengths = [int(line.strip()) for line in file.readlines()]
+    with open('lengths_and_durations_cache.txt', 'r') as file:
+        lengths_durations = [(int(line.strip().split(',')[0]), float(line.strip().split(',')[1]))  for line in file.readlines()]
 
 reference_architectures = {
     WaveNetTCNFBMModelClassifier: None,
@@ -30,7 +32,10 @@ reference_architectures = {
     WavenetTCNWithLSTMHurstExponentPredicter: {},
 }
 
-for index, length in tqdm.tqdm(list(enumerate(lengths))):
+for index, length_duration in tqdm.tqdm(list(enumerate(lengths_durations))):
+    print("Length,Duration:", length_duration)
+    length = length_duration[0]
+    duration = length_duration[1]
     clear_session()
     for network_class in [WaveNetTCNFBMModelClassifier, WaveNetTCNSBMModelClassifier, WavenetTCNWithLSTMHurstExponentPredicter]:
 
@@ -38,11 +43,10 @@ for index, length in tqdm.tqdm(list(enumerate(lengths))):
 
             for class_model in ALL_SUB_MODELS:
                 already_trained_networks = network_class.objects(simulator_identifier=CustomDataSimulation.STRING_LABEL, trained=True, hyperparameters=network_class.selected_hyperparameters(class_model.STRING_LABEL))
+                available_networks = [network for network in already_trained_networks if network.trajectory_length == length and (network.trajectory_time * 0.85 <= duration <= network.trajectory_time * 1.15) and network.extra_parameters['model'] == class_model.STRING_LABEL]
 
-                networks_of_length = [network for network in already_trained_networks if network.trajectory_length == length and network.extra_parameters['model'] == class_model.STRING_LABEL]
-
-                if len(networks_of_length) == 0:
-                    classifier = network_class(length, length * EXPERIMENT_TIME_FRAME_BY_FRAME, simulator=CustomDataSimulation, model=class_model.STRING_LABEL)
+                if len(available_networks) == 0:
+                    classifier = network_class(length, duration, simulator=CustomDataSimulation, model=class_model.STRING_LABEL)
 
                     if index == 0:
                         reference_architectures[network_class][class_model.STRING_LABEL] = classifier
@@ -54,24 +58,22 @@ for index, length in tqdm.tqdm(list(enumerate(lengths))):
                     classifier.fit()
                     classifier.save()
                 else:
-                    assert len(networks_of_length) == 1
-                    classifier = networks_of_length[0]
-
                     if index == 0:
-                        reference_architectures[network_class][class_model.STRING_LABEL] = classifier
-                    else:
-                        classifier.set_wadnet_tcn_encoder(reference_architectures[network_class][class_model.STRING_LABEL], -3)
+                        if len(available_networks) == 1:
+                            classifier = available_networks[0]
+                        else:
+                            classifier = max(*available_networks, key= lambda net: net.trajectory_time)
 
-                    classifier.enable_database_persistance()
-                    classifier.load_as_file()
+                        reference_architectures[network_class][class_model.STRING_LABEL] = classifier
+                        classifier.enable_database_persistance()
+                        classifier.load_as_file()
 
         else:
             already_trained_networks = network_class.objects(simulator_identifier=CustomDataSimulation.STRING_LABEL, trained=True, hyperparameters=network_class.selected_hyperparameters())
+            available_networks = [network for network in already_trained_networks if network.trajectory_length == length and (network.trajectory_time * 0.85 <= duration <= network.trajectory_time * 1.15)]
 
-            networks_of_length = [network for network in already_trained_networks if network.trajectory_length == length]
-
-            if len(networks_of_length) == 0:
-                classifier = network_class(length, length * EXPERIMENT_TIME_FRAME_BY_FRAME, simulator=CustomDataSimulation)
+            if len(available_networks) == 0:
+                classifier = network_class(length, duration, simulator=CustomDataSimulation)
 
                 if index == 0:
                     reference_architectures[network_class] = classifier
@@ -83,16 +85,15 @@ for index, length in tqdm.tqdm(list(enumerate(lengths))):
                 classifier.fit()
                 classifier.save()
             else:
-                assert len(networks_of_length) == 1
-                classifier = networks_of_length[0]
-
                 if index == 0:
-                    reference_architectures[network_class] = classifier
-                else:
-                    classifier.set_wadnet_tcn_encoder(reference_architectures[network_class], -4)
+                    if len(available_networks) == 1:
+                        classifier = available_networks[0]
+                    else:
+                        classifier = max(*available_networks, key= lambda net: net.trajectory_time)
 
-                classifier.enable_database_persistance()
-                classifier.load_as_file()
+                    reference_architectures[network_class] = classifier
+                    classifier.enable_database_persistance()
+                    classifier.load_as_file()
 
 
 DatabaseHandler.disconnect()
