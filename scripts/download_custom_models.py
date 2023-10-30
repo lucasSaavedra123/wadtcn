4481037eb408dfe7a5faf6f5916c0fd51cb6a97c
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2023-10-29 21:54:17 -0300

    Script updated

--- scripts/download_custom_models.py
+++ scripts/download_custom_models.py
@@ -15,21 +15,21 @@ regression_trained_networks = list(WavenetTCNWithLSTMHurstExponentPredicter.obje
 regression_trained_networks = sorted(regression_trained_networks, key=lambda net: (net.trajectory_length, -net.trajectory_time))
 reference_network = regression_trained_networks[0]
 
-reference_classification_trained_networks = [network for network in regression_trained_networks if network.trajectory_length == reference_network.trajectory_length and network.trajectory_time == reference_network.trajectory_time]
+reference_regression_trained_networks = [network for network in regression_trained_networks if network.trajectory_length == reference_network.trajectory_length and network.trajectory_time == reference_network.trajectory_time]
 
-for reference_network in tqdm.tqdm(reference_classification_trained_networks):
+for reference_network in tqdm.tqdm(reference_regression_trained_networks):
     reference_network.enable_database_persistance()
     reference_network.load_as_file()
     reference_network.disable_database_persistance()
     reference_network.save_as_file()
 
-for transfer_learning_network in tqdm.tqdm([n for n in regression_trained_networks if n not in reference_classification_trained_networks]):
-    reference_network = [network for network in reference_classification_trained_networks if transfer_learning_network.extra_parameters['model'] == network.extra_parameters['model']][0]
+for transfer_learning_network in tqdm.tqdm([n for n in regression_trained_networks if n not in reference_regression_trained_networks]):
+    reference_network = [network for network in reference_regression_trained_networks if transfer_learning_network.extra_parameters['model'] == network.extra_parameters['model']][0]
     transfer_learning_network.set_wadnet_tcn_encoder(reference_network, -3)
-    reference_network.enable_database_persistance()
-    reference_network.load_as_file()
-    reference_network.disable_database_persistance()
-    reference_network.save_as_file()
+    transfer_learning_network.enable_database_persistance()
+    transfer_learning_network.load_as_file()
+    transfer_learning_network.disable_database_persistance()
+    transfer_learning_network.save_as_file()
 
 print("Downloading Model Classification Networks...")
 classification_trained_networks = list(WaveNetTCNTheoreticalModelClassifier.objects(simulator_identifier=CustomDataSimulation.STRING_LABEL, trained=True, hyperparameters=WaveNetTCNTheoreticalModelClassifier.selected_hyperparameters()))
