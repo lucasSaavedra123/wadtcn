2133343e55a04a5212a11f2b9982937d11e5ecd2
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-03-30 22:18:27 -0300

    flag added

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,6 +1,8 @@
 TRAINING_SET_SIZE_PER_EPOCH = 100000
 VALIDATION_SET_SIZE_PER_EPOCH = 12500
 
+SIMULATE_FOR_MINFLUX = False
+
 EXPERIMENT_PIXEL_SIZE = 106 #Each px has size 106nm
 EXPERIMENT_HEIGHT = 10000 #Experiments were done with images of 10000nm of height
 EXPERIMENT_WIDTH = 10000 #Experiments were done with images of 10000nm of width
