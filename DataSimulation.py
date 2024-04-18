b2c472acc8250785ef76cbf97ee6e268fb924831
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-04-17 23:31:33 -0300

    DataSimulation updated

--- DataSimulation.py
+++ DataSimulation.py
@@ -1,7 +1,9 @@
 from random import shuffle
 from numpy.random import choice
 import numpy as np
-from andi_datasets.datasets_challenge import challenge_theory_dataset
+from andi_datasets.datasets_challenge import challenge_theory_dataset, _get_dic_andi2, _defaults_andi2
+from andi_datasets.datasets_phenom import datasets_phenom
+
 from Trajectory import Trajectory
 
 class DataSimulation():
@@ -83,3 +85,70 @@ class AndiDataSimulation(DataSimulation):
             )
 
         return trajectories
+
+class Andi2ndDataSimulation(DataSimulation):
+    STRING_LABEL = 'andi2'
+
+    def __init__(self):
+        self.andi = True
+
+    def simulate_phenomenological_trajectories(self, number_of_trajectories, trajectory_length, trajectory_time):
+        EXPERIMENTS = np.arange(5).repeat(2)
+        NUM_FOVS = 10
+
+        # We create a list of dictionaries with the properties of each experiment
+        exp_dic = [None]*len(EXPERIMENTS)
+
+        ##### SINGLE STATE #####
+        exp_dic[0] = {'Ds': [1, 0.01], 'alphas' : [0.5, 0.01]}
+        exp_dic[1] = {'Ds': [0.1, 0.01], 'alphas' : [1.9, 0.01]}
+
+        ##### MULTI STATE #####
+        exp_dic[2] = {'Ds': np.array([[1, 0.01], [0.05, 0.01]]),
+                    'alphas' : np.array([[1.5, 0.01], [0.5, 0.01]]),
+                    'M': np.array([[0.99, 0.01],[0.01, 0.99]])
+                    }
+        exp_dic[3] = {'Ds': np.array([[1, 0.01], [0.5, 0.01], [0.01, 0.01]]),
+                    'alphas' : np.array([[1.5, 0.01], [0.5, 0.01], [0.75, 0.01]]),
+                    'M': np.array([[0.98, 0.01, 0.01],[0.01, 0.98, 0.01], [0.01, 0.01, 0.98]])
+                    }
+
+        ##### IMMOBILE TRAPS #####
+        exp_dic[4] = {'Ds': [1, 0.01], 'alphas' : [0.8, 0.01],
+                    'Pu': 0.01, 'Pb': 1,
+                    'Nt': 300, 'r': 0.6}
+        exp_dic[5] = {'Ds': [1, 0.01], 'alphas' : [1.5, 0.01],
+                    'Pu': 0.05, 'Pb': 1,
+                    'Nt': 150, 'r': 1}
+
+        ##### DIMERIZATION #####
+        exp_dic[6] = {'Ds': np.array([[1, 0.01], [1, 0.01]]), 'alphas' : np.array([[1.2, 0.01], [0.8, 0.01]]),
+                    'Pu': 0.01, 'Pb': 1,  'N': 100, 'r': 0.6}
+        exp_dic[7] = {'Ds': np.array([[1, 0.01], [3, 0.01]]), 'alphas' : np.array([[1.2, 0.01], [0.5, 0.01]]),
+                    'Pu': 0.01, 'Pb': 1,  'N': 80, 'r': 1}
+
+        ##### CONFINEMENT #####
+        exp_dic[8] = {'Ds': np.array([[1, 0.01], [1, 0.01]]), 'alphas' : np.array([[0.8, 0.01], [0.4, 0.01]]),
+                    'Nc': 30, 'trans': 0.1, 'r': 5}
+        exp_dic[9] = {'Ds': np.array([[1, 0.01], [0.1, 0.01]]), 'alphas' : np.array([[1, 0.01], [1, 0.01]]),
+                    'Nc': 30, 'trans': 0, 'r': 10}
+
+        trajectories = []
+
+        while len(trajectories) < number_of_trajectories:
+            idx = np.random.randint(0, len(EXPERIMENTS))
+            i = EXPERIMENTS[idx]
+            fix_exp = exp_dic[idx]
+
+            dic = _get_dic_andi2(i+1)
+            dic['T'] = trajectory_length
+            dic['N'] = 100
+
+            for key in fix_exp:
+                dic[key] = fix_exp[key]
+
+            for _ in range(NUM_FOVS):
+                trajs, labels = datasets_phenom().create_dataset(dics = dic)
+                trajectories += [ti for ti in Trajectory.from_datasets_phenom(trajs, labels) if len(np.unique(ti.info['alpha_t'])) != 1 and 0 not in ti.info['alpha_t']] #We want a diverse number of characteristics
+
+        return trajectories[:number_of_trajectories]
