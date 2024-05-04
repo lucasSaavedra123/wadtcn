a9121e01544a4ac33e11b8474d8e7f8131e4e7ee
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2024-05-04 11:37:06 -0300

    analysis updated

--- scripts/2nd_andi_challenge_hyperparameter_analysis.py
+++ scripts/2nd_andi_challenge_hyperparameter_analysis.py
@@ -6,6 +6,6 @@ from DatabaseHandler import DatabaseHandler
 
 L = 200
 DatabaseHandler.connect_over_network(None, None, 'localhost', 'anomalous_diffusion')
-WavenetTCNMultiTaskSingleLevelPredicter.analyze_hyperparameters(L, None, simulator=Andi2ndDataSimulation)
-WavenetTCNMultiTaskSingleLevelPredicter.plot_hyperparameter_search(L, None, simulator=Andi2ndDataSimulation)
+WavenetTCNMultiTaskSingleLevelPredicter.analyze_hyperparameters(L, L, simulator=Andi2ndDataSimulation, initial_epochs=1, steps=1)
+WavenetTCNMultiTaskSingleLevelPredicter.plot_hyperparameter_search(L, L, simulator=Andi2ndDataSimulation)
 DatabaseHandler.disconnect()
