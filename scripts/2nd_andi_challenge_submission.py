598ece435ee47de15d6bcb7c50f72d01fdc34731
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-06-23 18:20:48 -0300

    Code Updated

--- scripts/2nd_andi_challenge_submission.py
+++ scripts/2nd_andi_challenge_submission.py
@@ -19,7 +19,7 @@ PUBLIC_DATA_PATH = './public_data_validation_v1'
 RESULT_PATH = './2nd_andi_challenge_results'
 PATH_TRACK_1, PATH_TRACK_2 = './track_1', './track_2'
 
-N_EXP = 10
+N_EXP = 13
 N_FOVS = 30
 
 info_field_to_network = {
@@ -36,28 +36,25 @@ trajectories = []
 print("Loading trajectories...")
 for exp in tqdm.tqdm(list(range(N_EXP))):
     for fov in range(N_FOVS):
-        submission_file = join(RESULT_PATH, PATH_TRACK_2, f'exp_{exp}', f'fov_{fov}.txt')
-        makedirs(join(RESULT_PATH, PATH_TRACK_2, f'exp_{exp}'), exist_ok=True)
-        with open(submission_file, 'w') as f:
-            dataframe_path = join(PUBLIC_DATA_PATH, PATH_TRACK_2, f'exp_{exp}', f'trajs_fov_{fov}.csv')
-            df = pd.read_csv(dataframe_path)
+        dataframe_path = join(PUBLIC_DATA_PATH, PATH_TRACK_2, f'exp_{exp}', f'trajs_fov_{fov}.csv')
+        df = pd.read_csv(dataframe_path)
 
-            for idx in df.traj_idx.unique():
-                df_traj = df[df.traj_idx == idx].sort_values('frame')
+        for idx in df.traj_idx.unique():
+            df_traj = df[df.traj_idx == idx].sort_values('frame')
 
-                trajectories.append(Trajectory(
-                    x = df_traj['x'].tolist(),
-                    y = df_traj['y'].tolist(),
-                    t = df_traj['frame'].tolist(),
-                    noisy=True,
-                    info={
-                        'idx': idx,
-                        'exp': exp,
-                        'fov': fov
-                    }
-                ))
+            trajectories.append(Trajectory(
+                x = df_traj['x'].tolist(),
+                y = df_traj['y'].tolist(),
+                t = df_traj['frame'].tolist(),
+                noisy=True,
+                info={
+                    'idx': idx,
+                    'exp': exp,
+                    'fov': fov
+                }
+            ))
 
-                #plt.plot(df_traj['x'].tolist(), df_traj['y'].tolist())
+            #plt.plot(df_traj['x'].tolist(), df_traj['y'].tolist())
         #plt.show()
 
 print("Number of trajectories:", len(trajectories))
@@ -78,34 +75,30 @@ for trajectory_length in tqdm.tqdm(trajectories_by_length):
         if field == 'state_t':
             prediction = np.argmax(prediction,axis=2)
         for t_i, t in enumerate(trajectories_by_length[trajectory_length]):
-            t.info[field] = prediction[t_i].flatten()*2 if field == 'alpha_t' else prediction[t_i].flatten()
+            flatten_input = prediction[t_i].flatten()
+            assert len(flatten_input) == t.length
+            if field == 'alpha_t':
+                t.info[field] = flatten_input*2
+            elif field == 'd_t':
+                t.info[field] = 10**flatten_input
+            else:
+                t.info[field] = flatten_input
 
 #Pointwise predictions are converted into segments and results are saved
 for exp in tqdm.tqdm(list(range(N_EXP))):
     for fov in range(N_FOVS):
-        submission_file = join(RESULT_PATH, PATH_TRACK_2, f'exp_{exp}', f'fov_{fov}.txt')
         makedirs(join(RESULT_PATH, PATH_TRACK_2, f'exp_{exp}'), exist_ok=True)
+        submission_file = join(RESULT_PATH, PATH_TRACK_2, f'exp_{exp}', f'fov_{fov}.txt')
         with open(submission_file, 'w') as f:
             exp_and_fov_trajectories = [t for t in trajectories if t.info['exp']==exp and t.info['fov']==fov]
             """
-            Here, breakpoints should be identified. By now, fake breakpoints are saved
+            Here, breakpoints are identified looking at the alpha signal
             """
-            result = rpt.Pelt(model="l1").fit(t.info[field]).predict(pen=1)
-
-            #fig, ax = plt.subplots(2,1)
-
-            #for index in result:
-            #    ax[0].axvline(index)
-
-            #ax[0].plot(t.info[field])
-            #ax[1].plot(np.diff(t.info[field]))
-            #ax[0].set_ylim([0,2])
-            #ax[1].set_ylim([-1,1])
-            #plt.show()
 
             for trajectory in exp_and_fov_trajectories:
                 prediction_traj = [int(trajectory.info['idx'])]
-                break_points = [int(0.75 * trajectory.length), trajectory.length]
+                break_points = rpt.Pelt(model="l1").fit(trajectory.info['alpha_t']).predict(pen=1)
+
                 last_break_point = 0
                 for bp in break_points:
                     prediction_traj += [
