ada56e5fba4c12a77f5cda6fce5cd09e57ba0046
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-07-04 17:01:50 -0300

    UNet Graphical validation updated

--- scripts/UNet graphical validation.py
+++ scripts/UNet graphical validation.py
@@ -9,7 +9,7 @@ from utils import tiff_movie_path_to_numpy_array, get_trajectories_from_2nd_andi
 
 DATA_PATH = './public_data_challenge_v0'
 EXP = 4
-FOV = 11
+FOV = 2
 
 network = UNetSingleParticleTracker(128,128,2)
 network.load_as_file()
@@ -27,17 +27,17 @@ while True:
 
     frame = movie[frame_index]
 
-    for row_index, row in dataframe[dataframe['frame'] == frame_index].iterrows():
-        color = (255,0,255) if row.vip else (0,0,255)
-        frame = cv2.circle(frame, (int(row.x.real),int(row.y.real)), radius=1, color=color)
-        #frame = cv2.putText(frame,str(row.traj_idx),(int(row.x.real),int(row.y.real)),cv2.FONT_HERSHEY_SIMPLEX,0.20,(0,255,0),1,cv2.LINE_AA)
-
     up_to_frame = dataframe[dataframe['frame'] <= frame_index]
-    
+
     for track_id in up_to_frame['traj_idx'].unique():
         track = up_to_frame[up_to_frame['traj_idx']==track_id].sort_values('frame')
         draw_points = (np.asarray([track['x'].values, track['y'].values]).T).astype(np.int32)
         frame = cv2.polylines(frame, [draw_points], False, (0,0,0))
+
+    for row_index, row in dataframe[dataframe['frame'] == frame_index].iterrows():
+        color = (255,0,255) if row.vip else (0,0,255)
+        frame = cv2.circle(frame, (int(row.x.real),int(row.y.real)), radius=1, color=color)
+        #frame = cv2.putText(frame,str(row.traj_idx),(int(row.x.real),int(row.y.real)),cv2.FONT_HERSHEY_SIMPLEX,0.20,(0,255,0),1,cv2.LINE_AA)
 
     #for an_id in id_to_pixel_position:
     #    frame = cv2.circle(frame, (int(id_to_pixel_position[an_id]['x'][0]),int(id_to_pixel_position[an_id]['y'][0])), radius=1, color=(255,255,0))
