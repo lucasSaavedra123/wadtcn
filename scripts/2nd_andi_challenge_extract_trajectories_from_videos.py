47bf93d7710c43da04431fe0f77ba3cc1b757e02
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2024-07-04 13:27:42 -0300

    Extraction updated

--- scripts/2nd_andi_challenge_extract_trajectories_from_videos.py
+++ scripts/2nd_andi_challenge_extract_trajectories_from_videos.py
@@ -8,7 +8,7 @@ from utils import tiff_movie_path_to_numpy_array, get_trajectories_from_2nd_andi
 PUBLIC_DATA_PATH = './public_data_challenge_v0'
 PATH_TRACK_1 = './track_1'
 
-N_EXP = 10
+N_EXP = 12
 N_FOVS = 30
 
 unet_network = UNetSingleParticleTracker(128,128,2)
@@ -19,8 +19,5 @@ for exp in tqdm.tqdm(list(range(N_EXP))):
         print(exp,fov)
         tiff_file_path = join(PUBLIC_DATA_PATH, PATH_TRACK_1, f'exp_{exp}', f'videos_fov_{fov}.tiff')
         tiff_movie = tiff_movie_path_to_numpy_array(tiff_file_path)
-        #if exp==4:
-        #    dataframe = get_trajectories_from_2nd_andi_challenge_tiff_movie(tiff_movie, unet_network, spt_max_distance_tolerance=7)
-        #else:
         dataframe = get_trajectories_from_2nd_andi_challenge_tiff_movie(tiff_movie, unet_network)
         dataframe.to_csv(join(PUBLIC_DATA_PATH, PATH_TRACK_1, f'exp_{exp}', f'trajs_fov_{fov}.csv'), index=False)
