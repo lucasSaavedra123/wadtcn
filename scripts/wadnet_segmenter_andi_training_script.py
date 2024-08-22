ce33bdb9ec00dfbafa1381a5ef02cc4cc276d63d
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2024-08-22 13:22:44 -0300

    script updated

--- scripts/wadnet_segmenter_andi_training_script.py
+++ scripts/wadnet_segmenter_andi_training_script.py
@@ -3,21 +3,17 @@ from PredictiveModel.WavenetTCNSingleLevelAlphaPredicter import WavenetTCNSingle
 from PredictiveModel.WavenetTCNMultiTaskClassifierSingleLevelPredicter import WavenetTCNMultiTaskClassifierSingleLevelPredicter
 from PredictiveModel.WavenetTCNSingleLevelChangePointPredicter import WavenetTCNSingleLevelChangePointPredicter
 
-"""
 net = WavenetTCNSingleLevelAlphaPredicter(200,200,simulator=AndiDataSimulation)
 net.enable_early_stopping()
 net.fit()
 net.save_as_file()
-"""
-"""
+
 net = WavenetTCNMultiTaskClassifierSingleLevelPredicter(200,200,simulator=AndiDataSimulation)
 net.enable_early_stopping()
 net.fit()
 net.save_as_file()
-"""
-"""
+
 net = WavenetTCNSingleLevelChangePointPredicter(200,200,simulator=AndiDataSimulation)
 net.enable_early_stopping()
 net.fit()
 net.save_as_file()
-"""
