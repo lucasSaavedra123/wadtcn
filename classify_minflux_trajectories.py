9e789ac0c377c36a6d986b2945cd5fedd5afaf9c
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2025-07-03 20:35:04 -0300

    Code updated

--- classify_minflux_trajectories.py
+++ classify_minflux_trajectories.py
@@ -10,10 +10,10 @@ import matplotlib.patches as mpatches
 
 DatabaseHandler.connect_over_network(None, None, 'localhost', 'MINFLUX_DATA')
 
-#network = WavenetTCNMultiTaskClassifierSingleLevelPredicter(1000,1000,simulator=DeepSPTDataSimulation)
-#network.load_as_file('wavenet_minflux.weights.h5')
-network = RunAndTurnSegmentator(200,200,simulator=CustomDataSimulation)
-network.load_as_file('run_and_turn_minflux.weights.h5')
+network = WavenetTCNMultiTaskClassifierSingleLevelPredicter(1000,1000,simulator=DeepSPTDataSimulation)
+network.load_as_file('wavenet_minflux.weights.h5')
+#network = RunAndTurnSegmentator(200,200,simulator=CustomDataSimulation)
+#network.load_as_file('run_and_turn_minflux.weights.h5')
 
 
 def delete_short_changes(signal, umbral=5):
@@ -40,21 +40,17 @@ for trajectory in Trajectory.objects():
     prediction = network.predict([trajectory])[0]
 
     prediction = prediction.argmax(axis=-1)
-
-    if 0 not in prediction:
-        continue
-
-    #prediction = delete_short_changes(prediction, umbral=25)
-    #trajectory.info['analysis']['deepspt_segmenter_result'] = prediction.tolist()
+    prediction = delete_short_changes(prediction, umbral=25)
+    trajectory.info['analysis']['deepspt_segmenter_result'] = prediction.tolist()
     trajectory.save()
-
+    """
     x = trajectory.get_noisy_x().tolist()
     y = trajectory.get_noisy_y().tolist()
 
-    #state_to_color = {1:'red', 0:'black', 2:'green', 3:'orange'}
-    #state_to_label = {1:'directed', 0:'normal', 2:'confined', 3:'subdifussive'}
-    state_to_color = {1:'red', 0:'black'}
-    state_to_label = {1:'turn', 0:'run'}
+    state_to_color = {1:'red', 0:'black', 2:'green', 3:'orange'}
+    state_to_label = {1:'directed', 0:'normal', 2:'confined', 3:'subdifussive'}
+    #state_to_color = {1:'red', 0:'black'}
+    #state_to_label = {1:'turn', 0:'run'}
     states_as_color = np.vectorize(state_to_color.get)(prediction)
 
     for i,(x1, x2, y1,y2) in enumerate(zip(x, x[1:], y, y[1:])):
@@ -66,5 +62,6 @@ for trajectory in Trajectory.objects():
 
     plt.legend(handles=patches)
     plt.show()
+    """
 
 DatabaseHandler.disconnect()
